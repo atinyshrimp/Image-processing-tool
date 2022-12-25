e26ab59a2f4ee94d6fd8f4910417911ee8a5db01
Joyce <joyce.lapilus@edu.devinci.fr>
2022-12-25 15:03:18 +0100

    Complete translation of HeaderInfo.cs

--- Probleme_info_Joyce/HeaderInfo.cs
+++ Probleme_info_Joyce/HeaderInfo.cs
@@ -4,7 +4,7 @@ namespace PSI_Joyce
 {
 
     /// <summary>
-    /// Classe contenant toutes les informations des entêtes d'un fichier bitmap
+    /// Class containing all header information of a bitmap file
     /// </summary>
     public class HeaderInfo
     {
@@ -24,32 +24,32 @@ namespace PSI_Joyce
         #region Properties
 
         /// <summary>
-        /// Taille du fichier Bitmap (entêtes incluses)
+        /// Bitmap file size (including headers)
         /// </summary>
         public int Size { get => size; }
 
         /// <summary>
-        /// L'indice où commence le bitmap de l'image
+        /// The index where the bitmap of the image begins (54 in most cases)
         /// </summary>
         public int SizeOffset { get => sizeOffset; }
 
         /// <summary>
-        /// Hauteur de l'image (en pixel)
+        /// Height of the image, given in pixels
         /// </summary>
         public int Height { get => height; }
 
         /// <summary>
-        /// Largeur de l'image (en pixel)
+        /// Width of the image, given in pixels
         /// </summary>
         public int Width { get => width; }
 
         /// <summary>
-        /// Nombre de 0 à compter pour enregistrer l'image adéqueatement au format Bitmap
+        /// Number of 0s to count to save the image properly in Bitmap file format
         /// </summary>
         public int Padding { get => padding; }
 
         /// <summary>
-        /// Tableau de bytes contenant tous les bytes des en-têtes du fichier bitmap
+        /// Byte array containing all header bytes of the bitmap file
         /// </summary>
         public byte[] Array { get => array; }
         #endregion
@@ -57,9 +57,9 @@ namespace PSI_Joyce
         #region Constructor(s)
 
         /// <summary>
-        /// Création de l'entête d'une image à partir de son chemin
+        /// Creating the header of an image from its path
         /// </summary>
-        /// <param name="file">Chemin menant au fichier .bmp à partir duquel l'entête sera créée</param>
+        /// <param name="file">Path leading to the .bmp file from which the header will be created</param>
         public HeaderInfo(string file)
         {
             if (file.Contains(".bmp"))
@@ -67,26 +67,26 @@ namespace PSI_Joyce
 
                 byte[] tab = File.ReadAllBytes(file);
 
-                byte[] ogSize = new byte[4]; //donné sur 4 octets
+                byte[] ogSize = new byte[4]; // given in 4 bytes
                 for (int i = 0; i < 4; i++) ogSize[i] = tab[2 + i];
                 size = MyImage.EndianToInt(ogSize);
 
-                byte[] ogSizeOffset = new byte[4]; //donné sur 4 octets
+                byte[] ogSizeOffset = new byte[4]; // given in 4 bytes
                 for (int i = 0; i < 4; i++) ogSizeOffset[i] = tab[10 + i];
                 sizeOffset = MyImage.EndianToInt(ogSizeOffset);
 
                 array = new byte[sizeOffset];
                 for (int i = 0; i < sizeOffset; i++) array[i] = tab[i];
 
-                byte[] ogWidth = new byte[4]; //donné sur 4 octets
+                byte[] ogWidth = new byte[4]; // given in 4 bytes
                 for (int i = 0; i < 4; i++) ogWidth[i] = tab[18 + i];
                 width = MyImage.EndianToInt(ogWidth);
 
-                byte[] ogHeight = new byte[4]; //donné sur 4 octets
+                byte[] ogHeight = new byte[4]; // given in 4 bytes
                 for (int i = 0; i < 4; i++) ogHeight[i] = tab[22 + i];
                 height = MyImage.EndianToInt(ogHeight);
 
-                byte[] ogBpc = new byte[2]; //donné sur 2 octets
+                byte[] ogBpc = new byte[2]; // given in 2 bytes
                 for (int i = 0; i < 2; i++) ogBpc[i] = tab[26 + i];
                 bpc = MyImage.EndianToInt(ogBpc);
 
@@ -95,15 +95,15 @@ namespace PSI_Joyce
         }
 
         /// <summary>
-        /// Création de l'entête d'une image à partir d'une matrice de Pixel
+        /// Creation of the header of an image from a Pixel matrix
         /// </summary>
-        /// <param name="pxm"></param>
+        /// <param name="pxm">Pixel array representing an image</param>
         /// 
         public HeaderInfo(Pixel[,] pxm)
         {
-            sizeOffset = 54; // tt le temps 54 ??
-            //pxm = new Pixel[pxm.GetLength(0), pxm.GetLength(1)];
-            height = pxm.GetLength(0); //nb de ligne
+            sizeOffset = 54; // most of the time 54
+            // pxm = new Pixel[pxm.GetLength(0), pxm.GetLength(1)];
+            height = pxm.GetLength(0); // nb of lines
             width = pxm.GetLength(1);
             bpc = 1;
             padding = width * 3 % 4;
@@ -117,8 +117,8 @@ namespace PSI_Joyce
             {
                 #region Header du fichier
 
-                array[2 + i] = MyImage.IntToEndian(size, 4)[i]; //taille du fichier bitmap (header inclus)
-                array[6 + i] = 0; //reserved, can be 0 if created manually
+                array[2 + i] = MyImage.IntToEndian(size, 4)[i]; // bitmap file size (including headers)
+                array[6 + i] = 0; // reserved, can be 0 if created manually
                 array[10 + i] = MyImage.IntToEndian(sizeOffset, 4)[i];
 
                 #endregion
@@ -128,20 +128,20 @@ namespace PSI_Joyce
                 array[14 + i] = MyImage.IntToEndian(40, 4)[i];
                 array[18 + i] = MyImage.IntToEndian(width, 4)[i];
                 array[22 + i] = MyImage.IntToEndian(height, 4)[i];
-                array[30 + i] = 0; //Compression BI_RGB ; la plus commune
-                array[34 + i] = MyImage.IntToEndian(width * height * 3 * bpc, 4)[i]; //taille de l'image en octets
-                array[38 + i] = MyImage.IntToEndian(11811, 4)[i]; //?? résolution horizontale par défaut de l'image "Coco"
-                array[42 + i] = MyImage.IntToEndian(11811, 4)[i]; //?? résolution verticale de l'image (pixel par mètre, cmmt savoir ça ?)
-                array[46 + i] = 0; //nombre de couleurs dans la palette de couleur ?? peut-être 0 aussi
-                array[50 + i] = 0; //nombre de couleurs "importantes" utilisées, 0 si elles le sont toutes
+                array[30 + i] = 0; // BI_RGB compression ; the most common
+                array[34 + i] = MyImage.IntToEndian(width * height * 3 * bpc, 4)[i]; // image size, in bytes
+                array[38 + i] = MyImage.IntToEndian(11811, 4)[i]; // default horizontal resolution
+                array[42 + i] = MyImage.IntToEndian(11811, 4)[i]; // default vertical resolution (pixel per meter)
+                array[46 + i] = 0; // nb of colors within the color palette ?? can also be 0
+                array[50 + i] = 0; // nb of "important" colors used, 0 if they are all important
 
                 #endregion
             }
 
             for (int i = 0; i < 2; i++)
             {
-                array[26 + i] = MyImage.IntToEndian(1, 2)[i]; //bit par couleur
-                array[28 + i] = MyImage.IntToEndian(24, 2)[i]; //pour une image de 24bit
+                array[26 + i] = MyImage.IntToEndian(1, 2)[i]; // bit per color
+                array[28 + i] = MyImage.IntToEndian(24, 2)[i]; // for a 24bit image
             }
 
 
